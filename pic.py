594cb60eb0e093ba1c0018371b37c0958eafe538
Znichu <znichu1989@gmail.com>
2019-10-18 12:44:09 +0300

    first

---
+++ pic.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import glob, os
+
+size = 988, 988
+
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile)
+    im.thumbnail(size, Image.ANTIALIAS)
+    im.save(file + ".JPEG", optimize=True)
+
+for infile in glob.glob("*.JPG"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile)
+    im.thumbnail(size, Image.ANTIALIAS)
+    im.save(file + ".JPEG", optimize=True)
