6cc91677b74d4322b4257b4f2ea35ac510d2cfbf
Znichu <znichu1989@gmail.com>
2019-10-24 10:59:34 +0300

    update

--- pic.py
+++ pic.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 from PIL import Image
 import glob, os
 
@@ -9,8 +12,18 @@ for infile in glob.glob("*.jpg"):
     im.thumbnail(size, Image.ANTIALIAS)
     im.save(file + ".JPEG", optimize=True)
 
+filelist = glob.glob("*.jpg")
+for f in filelist:
+    os.remove(f)    
+
 for infile in glob.glob("*.JPG"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
     im.thumbnail(size, Image.ANTIALIAS)
     im.save(file + ".JPEG", optimize=True)
+
+filelist = glob.glob("*.JPG")
+for f in filelist:
+    os.remove(f)
+
+
