c9e3234c2923614547f46ff7b4e74cc806d77356
Znichu <znichu1989@gmail.com>
2019-11-05 12:08:47 +0300

    Add work with .docx

--- pic.py
+++ pic.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-
+from bs4 import BeautifulSoup
+import mammoth
 from PIL import Image
 import glob, os
+
 
 size = 988, 988
 
@@ -41,6 +43,30 @@ for f in filelist:
     os.remove(f)
 
 
+
+
+for infile in glob.glob("*.docx"):
+    file, ext = os.path.splitext(infile)
+    Docx = open(infile, 'rb')
+    html = open("out.html", 'wb')
+    document = mammoth.convert_to_html(Docx)
+    html.write(document.value.encode('utf8'))
+    Docx.close()
+    html.close()
+
+with open('out.html', 'r') as file:
+    fcontent = file.read()
+
+soup = BeautifulSoup(fcontent, 'html.parser')
+tag = soup.p
+
+for tag in soup.find_all('p'):
+    tag_name = 'p'
+    tag['style'] = 'ext-align: justify; text-indent: 20px;'
+print ('Done!!!') 
+
+with open('out.html', 'w') as fp:
+    fp.write(soup.prettify())
 
 
 
